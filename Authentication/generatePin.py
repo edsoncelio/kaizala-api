a70e5ebdd48dd4b3dfccce71a313124c3caa76ee
Edson Celio <edsoncelio@protonmail.com>
2018-12-19 15:06:42 -0300

    removing credentials

--- Authentication/generatePin.py
+++ Authentication/generatePin.py
@@ -2,8 +2,7 @@ import requests
 
 url = "https://api.kaiza.la/v1/generatePin"
 
-payload = "{\"mobileNumber\":\"+919910870005\", 
-applicationId:\"37ba1e41-d55c-43bf-a2fa-d72c97aa080a\"}"
+payload = "{'mobileNumber':"",'applicationId':""}"
 headers = {
     'Content-Type': "application/json",
     'cache-control': "no-cache"
--- Authentication/loginWithPinAndApplicationId.py
+++ Authentication/loginWithPinAndApplicationId.py
@@ -2,9 +2,7 @@ import requests
 
 url = "https://api.kaiza.la/v1/loginWithPinAndApplicationId"
 
-payload = 
-"{\"mobileNumber\":\"+5585996058401\",\"applicationId\":\"37ba1e41-d55c-43bf-a2fa-d72c97aa080a\", 
-\"applicationSecret\":\"W2N7H1RW3V\", \"pin\":6823}"
+payload = "{'mobileNumber':"",'applicationId':"", 'applicationSecret':"", 'pin':6823}"
 headers = {
     'Content-Type': "application/json",
     'cache-control': "no-cache"
