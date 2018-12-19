53bdfe3759a48464f56c5aed10c5d0cd31060209
Edson Celio (aka tuxpilgrim) <edsoncelio@users.noreply.github.com>
2018-12-19 15:09:53 -0300

    Update loginWithPinAndApplicationId.py

--- Authentication/loginWithPinAndApplicationId.py
+++ Authentication/loginWithPinAndApplicationId.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.kaiza.la/v1/loginWithPinAndApplicationId"
 
-payload = "{'mobileNumber':"",'applicationId':"", 'applicationSecret':"", 'pin':6823}"
+payload = "{'mobileNumber':"",'applicationId':"", 'applicationSecret':"", 'pin':""}"
 headers = {
     'Content-Type': "application/json",
     'cache-control': "no-cache"
